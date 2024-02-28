919d26dc2b38fa3dcaa48a9d246b44d5163bf623
KXLVXN7 <kelvinshinx@gmail.com>
2024-02-28 19:32:46 +0700

    update

--- Assets/Script/Ground.cs
+++ Assets/Script/Ground.cs
@@ -5,21 +5,12 @@ public class Ground : MonoBehaviour
     public float groundSpeed = 5f; // Kecepatan pergerakan tanah ke kiri
     public float speedIncrement = 1f; // Penambahan kecepatan setiap 10 detik
     public float maxSpeedIncrement = 10f; // Jumlah maksimal penambahan kecepatan
-    public float destroyOffset = 20f; // Jarak dari layar kamera di mana tanah akan dihancurkan
 
-    private float screenRight;
-    private bool isClone = false;
     private float elapsedTime; // Waktu yang telah berlalu sejak penambahan terakhir
     private int speedIncreaseCount; // Jumlah penambahan kecepatan
 
     void Start()
     {
-        screenRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)).x;
-        if (transform.parent != null)
-        {
-            isClone = true;
-        }
-
         elapsedTime = 0f;
         speedIncreaseCount = 0;
     }
@@ -38,13 +29,6 @@ public class Ground : MonoBehaviour
             groundSpeed += speedIncrement;
             elapsedTime = 0f; // Reset waktu yang telah berlalu
             speedIncreaseCount++; // Tambah jumlah penambahan speed
-        }
-
-        // Jika Ground berada di luar layar kamera, hancurkan klon
-        if (isClone && transform.position.x < -screenRight)
-        {
-            Destroy(gameObject);
-            Debug.Log("Destroy Ground Clone");
         }
     }
 }
